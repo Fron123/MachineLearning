d7ad2fdf4e59261d6d4c569d385b5484db232e8e
Rico Stanosek <rico.stanosek@alumni.fh-aachen.de>
2021-11-23 19:14:26 +0100

    a2 weitergemacht

--- exercise-02/q2_svm_python/svmlin.py
+++ exercise-02/q2_svm_python/svmlin.py
@@ -4,7 +4,6 @@ import numpy as np
 # os.environ["PATH"] += os.pathsep + ...
 import cvxopt
 
-
 def svmlin(X, t, C):
     # Linear SVM Classifier
     #
@@ -21,9 +20,37 @@ def svmlin(X, t, C):
     # result   : result of classification     (1 x num_samples)
     # slack    : points inside the margin (boolean)   (1 x num_samples)
 
-    print("X", X)
-    print("t", t)
-    print("C", C)
+    N = len(t)
+    # print("X", X)
+    # print("t", t)
+    # print("C", C)
+    # print("N", N)
+
+    vec_C = np.array([C for _ in range(N)])
+    vec_1 = np.ones(N)
+    vec_0 = np.zeros(N)
+    H = np.zeros(shape=(N, N))
+
+    for n in range(N):
+        for m in range(N):
+            H[n][m] = t[n] * t[m] * np.inner(X[n], X[m])
+
+    print(H)
+
+    n = H.shape[1]
+    LB = vec_0
+    UB = vec_C
+
+    q = cvxopt.matrix((-1) * np.ones(N))
+    G = cvxopt.matrix(np.vstack([-np.eye(n), np.eye(n)]))
+    A = cvxopt.matrix(t)
+    b = cvxopt.matrix(0)
+    P = cvxopt.matrix(X)
+    h = cvxopt.matrix(np.hstack([-LB, UB]))
+
+    a = cvxopt.solvers.qp(H, q, G, h, A, b)
+
+
 
     pass
     # return alpha, sv, w, b, result, slack
